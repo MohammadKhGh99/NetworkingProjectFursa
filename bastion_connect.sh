873dee375635aca6f6e553ddd46ec3b6c703fbb8
MohammadKhGh99 <m7md.gh.99@hotmail.com>
2024-05-23 00:41:43 +0300

    section 1 & 2 Done (I think!)

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+if [ -z "${KEY_PATH}" ]; then
+  echo "KEY_PATH env var is expected"
+  exit 5
+fi
+
+if [ $# -eq 1 ]; then
+  ssh -i "$KEY_PATH" ubuntu@$1
+elif [ $# -eq 2 ]; then
+  ssh -o StrictHostKeyChecking=accept-new -t -i "$KEY_PATH" ubuntu@$1 "ssh -i machine1.pem ubuntu@$2"
+elif [ $# -eq 3 ];then
+  ssh -o StrictHostKeyChecking=accept-new -i "$KEY_PATH" ubuntu@$1 "ssh -i machine1.pem ubuntu@$2 '$3'"
+else
+  echo "Please provide bastion IP address"
+  exit 5
+fi
