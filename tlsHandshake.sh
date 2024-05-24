de3414486c5479f7d4bf0a65b3b68180cffd91bf
MohammadKhGh99 <m7md.gh.99@hotmail.com>
2024-05-24 19:51:43 +0300

    testing

--- tlsHandshake.sh
+++ tlsHandshake.sh
@@ -45,7 +45,7 @@ KEY_EXCH="{'sessionID': $SESSION_ID, 'masterKey': $MASTER_KEY, 'sampleMessage', 
 
 # get the encrypted sample message from server
 ENC_SAM_MSG=$(curl -X POST -H "Content-Type: application/json" -d "$KEY_EXCH" "$1:8080/keyexchange" | jq -r ".encryptedSampleMessage")
-
+curl -X POST -H "Content-Type: application/json" -d "$KEY_EXCH" "$1:8080/keyexchange"
 # decode the message
 DEC_SAM_MSG=$(base64 -d "$ENC_SAM_MSG" | openssl enc -d -aes-256-cbc -pbkdf2 -k "$MASTER_KEY")
 
